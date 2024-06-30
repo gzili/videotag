0833f30719d1f79da74fe8ed13ea18afbc789346
gzili <gustas.zilinskas@gmail.com>
2024-06-30 17:27:31 +0300

    include hours in formatDuration

--- videoTag.client/src/utils.ts
+++ videoTag.client/src/utils.ts
@@ -1,7 +1,15 @@
 export function formatDuration(seconds: number) {
-  const minutes = Math.floor(seconds / 60);
-  const secondsLeft = seconds - minutes * 60;
-  return `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`
+  let secondsRemaining = seconds;
+
+  const hours = Math.floor(seconds / 3600);
+  secondsRemaining -= hours * 3600;
+
+  const minutes = Math.floor(secondsRemaining / 60);
+  secondsRemaining -= minutes * 60;
+
+  const prefix = hours ? `${hours}:${minutes < 10 ? '0' : ''}` : '';
+
+  return `${prefix}${minutes}:${secondsRemaining < 10 ? '0' : ''}${secondsRemaining}`;
 }
 
 export function formatSize(bytes: number) {
@@ -12,6 +20,6 @@ export function formatSize(bytes: number) {
   } else if (bytes >= 1024) {
     return `${(bytes / 1024).toFixed(0)} KB`;
   } else {
-    return `${bytes} bytes`
+    return `${bytes} bytes`;
   }
 }
