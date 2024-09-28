a183c754c942fd9127d7f988eed2be2c82789a9c
Gustas Žilinskas <gustas.zilinskas@gmail.com>
2024-09-28 22:02:52 +0300

    1.1.1 (#3)
    * ensure correct video resolution format
    
    * update config files

--- VideoTag.Server/Helpers/Ffprobe.cs
+++ VideoTag.Server/Helpers/Ffprobe.cs
@@ -1,7 +1,11 @@
+using System.Text.RegularExpressions;
+
 namespace VideoTag.Server.Helpers;
 
 public static class Ffprobe
 {
+    private static readonly Regex VideoResolutionRegex = new(@"^\d+x\d+");
+    
     public static async Task<int> GetVideoDurationInSecondsAsync(string videoPath)
     {
         var arguments = $"-v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 \"{videoPath}\"";
@@ -12,6 +16,10 @@ public static class Ffprobe
     public static async Task<string> GetVideoResolutionAsync(string videoPath)
     {
         var arguments = $"-v error -select_streams v:0 -show_entries stream=height,width -of csv=s=x:p=0 \"{videoPath}\"";
-        return await ProcessAsyncHelper.RunProcessAndReadStringAsync("ffprobe", arguments);
+        var output = await ProcessAsyncHelper.RunProcessAndReadStringAsync("ffprobe", arguments);
+        var match = VideoResolutionRegex.Match(output);
+        if (!match.Success)
+            throw new Exception("Invalid video resolution");
+        return match.Value;
     }
 }
